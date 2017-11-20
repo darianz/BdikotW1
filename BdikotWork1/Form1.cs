341a50645d9fabeb8be9dcb88b1f1ee2b4af2291
ros stolero <ronst@ac.sce.ac.il>
2017-11-21 01:01:53 +0200

    making 5 workers and doing babble sort on the array to organize the workers based on salaries

--- BdikotWork1/Form1.cs
+++ BdikotWork1/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace BdikotWork1
 {
     public partial class Form1 : Form
@@ -23,13 +25,55 @@ namespace BdikotWork1
         private void Form1_Load(object sender, EventArgs e)
         {
             Random random = new Random();
-            Worker[] workersArray = new Worker[5];
-            for (int i = 0; i < 5; i++)
+            int SIZE = 5; 
+            Worker[] workersArray = new Worker[SIZE];
+            for (int i = 0; i < SIZE; i++)
             {
+
                 workersArray[i] = new Worker(i + 1, "worker" + i + 1, "worker" + i + 1, "worker" + 1 + i + "@gmail.com", random.Next(), "House number " + i + random.Next(), random.Next(15000, 300000));
+
+                //checking input for 5 workers
+                //MessageBox.Show("Worker number " + i + 1 + "Information:\n " + "ID: " + workersArray[i].ID + " Private Name: " + workersArray[i].PName + "Surname: " + workersArray[i].SName + "Email: " + workersArray[i].EMail + "Phone: " + workersArray[i].Phone + "Adress: " + workersArray[i].Adress + "Salary: " + workersArray[i].Salary);
+
+
                 
+
             }
-            
+            //PRINT SALARIES BEFORE BABBLE SORT
+
+            //string CheckArray = "The salaries are: \n";
+            //for (int i = 0; i < SIZE; i++)
+            //{
+            //    CheckArray += " " + workersArray[i].Salary + ", ";
+            //}
+            //MessageBox.Show(CheckArray);
+
+            // System.currentTimeMillis() - To mesure time
+            //Babble Sort
+                
+                
+                for (int i = workersArray.Length - 1; i > 0; i--)
+                {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (workersArray[j].Salary > workersArray[j + 1].Salary)
+                    {
+                        Worker highValue = workersArray[j];
+
+                        workersArray[j] = workersArray[j + 1];
+                        workersArray[j + 1] = highValue;
+                    }
+                }
+                }
+
+            //PRINT SALARIES AFTER BABBLE SORT
+
+            //string AfterCheck = "The salaries are: \n";
+            //for (int i = 0; i < SIZE; i++)
+            //{
+            //    AfterCheck += " " + workersArray[i].Salary + ", ";
+            //}
+            //MessageBox.Show(AfterCheck);
 
         }
 
