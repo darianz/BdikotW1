07548fbcc40b7f41740f87fef270e188a3a5b516
Ron Stoleru <darianz.ron@gmail.com>
2017-11-20 23:52:45 +0200

    bdika1

---
+++ BdikotWork1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BdikotWork1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+            
+          
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Worker[] workersArray = new Worker[5];
+            for (int i = 0; i < 5; i++)
+            {
+                workersArray[i] = new Worker(i + 1, "worker" + i + 1, "worker" + i + 1, "worker" + 1 + i + "@gmail.com", random.Next(), "House number " + i + random.Next(), random.Next(15000, 300000));
+                
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e )
+        {
+            
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BdikotWork1/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdikotWork1
+{
+    class Worker
+    {
+        public long ID;
+        public string PName;
+        public string SName;
+        public string EMail;
+        public long Phone;
+        public string Adress;
+        public long Salary;
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(long id , string pname, string sname , string email, long phone , string adress, long salary)
+        {
+            this.ID = id;
+            this.PName = pname;
+            this.SName = sname;
+            this.EMail = email;
+            this.Phone = phone;
+            this.Adress = adress;
+            this.Salary = salary;
+        }
+
+        
+    }
+}
